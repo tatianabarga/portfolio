f228e9c03b41721cbc6cf1a6e8f5cc180a62ad8e
Tatiana Barga <tatibspersonal@gmail.com>
2024-10-22 16:41:55 -0500

    background configured for all screen sizes and some other styling adjustments

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,23 @@
 /* eslint-disable react/prop-types */
 // import 'bootstrap/dist/css/bootstrap.min.css';
+// import Image from 'next/image';
 import NoAuth from '../components/NoAuth';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <NoAuth
-        component={Component}
-        pageProps={pageProps}
-      />
-    </>
+    <div className="app-cont">
+      {/* <div className="bg-img">
+        <Image src="/../public/background.jpg" width="100%" height="100%" />
+      </div> */}
+      <div className="auth-cont">
+        <NoAuth
+          component={Component}
+          pageProps={pageProps}
+        />
+      </div>
+
+    </div>
   );
 }
 
