a467513d7d5e4961315078662a5c36b9a1daa882
Tatiana Barga <tatibspersonal@gmail.com>
2024-11-05 21:22:06 -0600

    cleaned up zombie code in sass files

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,11 @@
 /* eslint-disable react/prop-types */
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import Image from 'next/image';
 import NoAuth from '../components/NoAuth';
 import '../styles/globals.scss';
-// import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="app-cont">
-      {/* <div className="bg-img">
-        <Image src="/../public/background.jpg" width="100%" height="100%" />
-      </div> */}
       <div className="auth-cont">
         <NoAuth
           component={Component}
