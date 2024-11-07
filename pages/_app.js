b080be093492126a8678bf27c21101890c10a1cc
Tatiana Barga <tatibspersonal@gmail.com>
2024-11-07 14:44:18 -0600

    replaced back up font for writing

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
       <div className="auth-cont">
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet" />
+          {/* <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet" /> */}
           <title>Tatiana Barga</title>
         </Head>
         <NoAuth
